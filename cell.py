6839ebd77fb4703b1649469e26141d5d732a1c41
Suzuha88 <didudun80@gmail.com>
2025-05-17 18:00:11 +0400

    Cell now draws over non-existant wall with canvas color if redrawn

--- cell.py
+++ cell.py
@@ -13,20 +13,37 @@ class Cell():
         self.__y1 = -1
         self.__y2 = -1
 
-        self.__win = window
+        self.__window = window
 
     def draw(self, p1: Point, p2: Point) -> None:
         self.__x1, self.__y1 = p1.x, p1.y
         self.__x2, self.__y2 = p2.x, p2.y
 
+        bg_color = self.__window.__canvas["background"]
+
         if self.has_left_wall:
-            self.__win.draw_line(Line(Point(p1.x, p1.y), Point(p1.x, p2.y)))
+            self.__window.draw_line(Line(Point(p1.x, p1.y), Point(p1.x, p2.y)))
+        else:
+            self.__window.draw_line(
+                Line(Point(p1.x, p1.y), Point(p1.x, p2.y)), bg_color)
+
         if self.has_bottom_wall:
-            self.__win.draw_line(Line(Point(p1.x, p2.y), Point(p2.x, p2.y)))
+            self.__window.draw_line(Line(Point(p1.x, p2.y), Point(p2.x, p2.y)))
+        else:
+            self.__window.draw_line(
+                Line(Point(p1.x, p2.y), Point(p2.x, p2.y)), bg_color)
+
         if self.has_top_wall:
-            self.__win.draw_line(Line(Point(p1.x, p1.y), Point(p2.x, p1.y)))
+            self.__window.draw_line(Line(Point(p1.x, p1.y), Point(p2.x, p1.y)))
+        else:
+            self.__window.draw_line(
+                Line(Point(p1.x, p1.y), Point(p2.x, p1.y)), bg_color)
+
         if self.has_right_wall:
-            self.__win.draw_line(Line(Point(p2.x, p1.y), Point(p2.x, p2.y)))
+            self.__window.draw_line(Line(Point(p2.x, p1.y), Point(p2.x, p2.y)))
+        else:
+            self.__window.draw_line(
+                Line(Point(p2.x, p1.y), Point(p2.x, p1.y)), bg_color)
 
     def draw_move(self, other: Self, undo=False) -> None:
         color = "gray" if undo else "red"
@@ -36,4 +53,4 @@ class Cell():
         finish = Point((other.__x1 + other.__x2) // 2,
                        (other.__y1 + other.__y2) // 2)
 
-        self.__win.draw_line(Line(start, finish), color)
+        self.__window.draw_line(Line(start, finish), color)
