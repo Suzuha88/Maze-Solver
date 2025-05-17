af3b3e0ec02ed2484539358595cb8be0403991c3
Suzuha88 <didudun80@gmail.com>
2025-05-17 15:06:15 +0400

    Created a method of Cell class to draw a line from one cell to another

--- graphics.py
+++ graphics.py
@@ -1,4 +1,5 @@
 from tkinter import Tk, BOTH, Canvas
+from typing import Self
 
 
 class Point():
@@ -72,3 +73,13 @@ class Cell():
             self.__win.draw_line(Line(Point(p1.x, p1.y), Point(p2.x, p1.y)))
         if self.has_right_wall:
             self.__win.draw_line(Line(Point(p2.x, p1.y), Point(p2.x, p2.y)))
+
+    def draw_move(self, other: Self, undo=False) -> None:
+        color = "gray" if undo else "red"
+
+        start = Point((self.__x1 + self.__x2) // 2,
+                      (self.__y1 + self.__y2) // 2)
+        finish = Point((other.__x1 + other.__x2) // 2,
+                       (other.__y1 + other.__y2) // 2)
+
+        self.__win.draw_line(Line(start, finish), color)
