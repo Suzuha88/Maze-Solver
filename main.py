ef9c952d322e2d80e83bd13961beb52694201902
Suzuha88 <didudun80@gmail.com>
2025-05-17 17:45:15 +0400

    Created Maze class to draw maze in the window

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import graphics as g
+import maze as m
 
 
 def main() -> None:
     window = g.Window(800, 600)
+
+    maze = m.Maze(g.Point(0, 0), 10, 10, 30, 30, window)
 
     window.wait_for_close()
 
---
+++ maze.py
@@ -0,0 +1,44 @@
+from time import sleep
+
+from graphics import *
+from cell import Cell
+
+
+class Maze():
+    def __init__(
+            self,
+            start: Point,
+            num_rows: int,
+            num_cols: int,
+            cell_width: int,
+            cell_height: int,
+            window: Window
+    ) -> None:
+        self.__start = start
+        self.__num_rows = num_rows
+        self.__num_cols = num_cols
+        self.__cell_width = cell_width
+        self.__cell_height = cell_height
+        self.__window = window
+
+        self.__cells = []
+        self.__create_cells()
+
+    def __create_cells(self) -> None:
+        for r in range(self.__num_rows):
+            self.__cells.append([])
+            for c in range(self.__num_cols):
+                self.__cells[-1].append(Cell(self.__window))
+                self.__draw_cell(r, c)
+
+    def __draw_cell(self, r: int, c: int) -> None:
+        top_left = Point(self.__start.x + c * self.__cell_width,
+                         self.__start.y + r * self.__cell_height)
+        bottom_right = Point(top_left.x + self.__cell_width,
+                             top_left.y + self.__cell_height)
+        self.__cells[r][c].draw(top_left, bottom_right)
+        self.__animate()
+
+    def __animate(self):
+        self.__window.redraw()
+        sleep(0.03)
